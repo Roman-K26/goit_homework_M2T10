c2a9268b64e16cbabbbe6f6da8090a310b050ff5
Roman-K26 <romankuspys777@gmail.com>
2023-12-05 19:19:43 +0300

    Commit 2

--- app_instagram/app_photo/urls.py
+++ app_instagram/app_photo/urls.py
@@ -6,5 +6,7 @@ from . import views
 app_name = "app_photo"
 
 urlpatterns = [
-    path('', views.index, name='home') # app_photo:home
+    path('', views.index, name='home'), # app_photo:home
+    path('images/', views.pictures, name='pictures'),
+    path('upload/', views.upload, name='upload')
 ]
--- app_instagram/app_photo/views.py
+++ app_instagram/app_photo/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from .forms import PictureForm
+from .models import Picture
 # Create your views here.
 
 def index(request):
     return render(request, template_name='app_photo/index.html', context={"msg": "Hello world"})
+
+def pictures(request):
+    pics = Picture.objects.all()
+    return render(request, template_name='app_photo/pictures.html', context={"pics": pics})
+
+def upload(request):
+    form = PictureForm(instance=Picture())
+    if request.method == 'POST':
+        form = PictureForm(request.POST, request.FILES, instance=Picture())
+        if form.is_valid():
+            form.save()
+            return redirect(to="app_photo:pictures")
+    return render(request, template_name='app_photo/upload.html', context={"form": form})
